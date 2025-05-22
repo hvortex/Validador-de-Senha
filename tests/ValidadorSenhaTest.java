3e486a9d831e35106537382f30d77b652e63568b
Arthur Verissimo <125267926+ArthurVGA1@users.noreply.github.com>
2025-05-22 13:59:55 -0300

    test: testando resultado de caractere especial

--- tests/ValidadorSenhaTest.java
+++ tests/ValidadorSenhaTest.java
@@ -5,7 +5,7 @@ public class ValidadorSenhaTest {
 
     @Test
     public void testSenhaSemLetraMaiuscula_DeveRetornarErro() {
-        ResultadoValidacao resultado = ValidadorSenha.validarSenha("abc12345!");
+        ResultadoValidacao resultado = ValidadorSenha.validarSenha("AAbc12345@#$!");
         assertFalse(resultado.isValida());
         assertTrue(resultado.getErros().contains("A senha deve conter pelo menos uma letra maiuscula"));
     }
